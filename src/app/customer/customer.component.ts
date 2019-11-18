541e5c602d2e45ad89fac7c129db29cc73f7b41a
winni4eva <adams4lyf@gmail.com>
2019-11-18 16:03:16 +0000

    en(form-builder): switched from form group to form builders

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 import { Customer } from './customer';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-customer',
@@ -14,21 +15,41 @@ export class CustomerComponent implements OnInit {
   // This is our data model, this matches the data passed to and from our server
   customer = new Customer();
 
-  constructor() { }
+  // Form builder is a service so we can utilize dependency injection to initialize it
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
     // This is our form model and helps us track our forms values and state
     // Matches up to our form structure in the template
-    this.customerForm = new FormGroup({
-      firstName: new FormControl(),
-      lastName: new FormControl(),
-      email: new FormControl(),
-      sendCatalog: new FormControl(true)
+    // Instead of using FormGroup we can use FormBuilder to simplify the form model
+    // this.customerForm = new FormGroup({
+    //   firstName: new FormControl(),
+    //   lastName: new FormControl(),
+    //   email: new FormControl(),
+    //   sendCatalog: new FormControl(true)
+    // });
+
+    this.customerForm = this.fb.group({
+      firstName: '',
+      lastName: '', //{value: 'n/a', disabled: true},
+      email: '',
+      sendCatalog: true
     });
   }
 
   save() {
     console.log(this.customerForm);
     console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+  }
+
+  populateTestData(): void {
+    // setValue is used to add data to every formControl in a forms model
+    // To update specific formControls we can use patchValue
+    this.customerForm.setValue({
+      firstName: 'Adam',
+      lastName: 'Winnipass',
+      email: 'adams4lyf@gmail.com',
+      sendCatalog: false
+    })
   }
 }
